8970fe2ea0ee0f35e3c6dfc293ef974135d34985
Souheil <souheilsouhil1@gmail.com>
2025-12-21 19:16:36 +0100

    Update RestaurantController.java

--- src/main/java/restaurantmanagementsystem/controller/RestaurantController.java
+++ src/main/java/restaurantmanagementsystem/controller/RestaurantController.java
@@ -1,4 +1,94 @@
 package restaurantmanagementsystem.controller;
+import java.util.Scanner;
+
 
 public class RestaurantController {
+
+        private Scanner scanner;
+
+        public RestaurantController() {
+            scanner = new Scanner(System.in);
+        }
+
+        public void start() {
+            boolean running = true;
+
+            while (running) {
+                showMainMenu();
+                int choice = readInt();
+
+                switch (choice) {
+                    case 1:
+                        handleShowMenu();
+                        break;
+                    case 2:
+                        handleCreateOrder();
+                        break;
+                    case 3:
+                        handlePayment();
+                        break;
+                    case 0:
+                        running = false;
+                        System.out.println("Goodbye!");
+                        break;
+                    default:
+                        System.out.println("Invalid choice.");
+                }
+            }
+
+            scanner.close();
+        }
+
+    private int readInt() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Please enter a valid number:");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    private void showMainMenu() {
+        System.out.println("=== RESTAURANT MANAGEMENT SYSTEM ===");
+        System.out.println("1. Show Menu");
+        System.out.println("2. Create Order");
+        System.out.println("3. Payment");
+        System.out.println("0. Exit");
+        System.out.print("Choice: ");
+    }
+
+
+    private void handleShowMenu() {
+        // MenuComponent menu = MenuComponent.getMenu();
+        // menu.display();
+
+        System.out.println("Menu displayed.");
+    }
+
+    private void handleCreateOrder() {
+        System.out.println("Creating a new order...");
+        // order.addItem(...)
+        // notify observers (automatic via model)
+    }
+
+    private void handlePayment() {
+        System.out.println("Choose payment method:");
+        System.out.println("1. Cash");
+        System.out.println("2. Card");
+
+        int choice = readInt();
+
+        switch (choice) {
+            case 1:
+                // paymentService.setStrategy(new CashPayment());
+                break;
+            case 2:
+                // paymentService.setStrategy(new CardPayment());
+                break;
+            default:
+                System.out.println("Invalid payment method.");
+        }
+
+        // paymentService.pay(order);
+    }
+
 }
